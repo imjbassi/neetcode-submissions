c03a48e2e3d4cf2c12cb306db8ba4220b1b4abfe
Jaiveer Bassi <jaiveerbassi@yahoo.com>
2026-08-06 19:54:32 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if (minSt.empty() || val <= minSt.top()) {
+            minSt.push(val);
+        } else {
+            minSt.push(minSt.top());
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+
+private:
+    stack<int> st;
+    stack<int> minSt;
+};
